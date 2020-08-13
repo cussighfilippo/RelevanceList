200ac026c9929a2936d2a70c4e215d5fad7cf830
flashwade <cussighfilippo@gmail.com>
2020-08-13 22:36:28 +0200

    fixed jar packaging

--- src/main/java/it/uniud/relevancelist/program/Program.java
+++ src/main/java/it/uniud/relevancelist/program/Program.java
@@ -42,7 +42,7 @@ public class Program
     {
 
     	
-    	//cmdline  java -jar .\target\RelevanceList-1.0-SNAPSHOT.jar 50 50000 0.8 0.3 50 1 0.8957 6 0.00005 10 "avgPrecision" "filepath" 0.1
+    	//cmdline  java -jar .\target\RelevanceList-1.0-SNAPSHOT.jar 50 50000 0.8 0.3 50 1 0.8957 6 0.00005 10 "avgPrecision" "risultati.csv" 0.1
 		// Lettura dei parametri
 		
 		if(args.length != 13){
